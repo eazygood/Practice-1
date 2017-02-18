97819aebb084dcd70da666a934859249f905ae2f
eazygood <kim.nac@hotmail.com>
2017-02-18 13:03:27 +0200

    added files

---
+++ app/js/common.js
@@ -0,0 +1,35 @@
+$(function() {
+	$(".toggle_menu").click(function() {
+		$(this).toggleClass("on");
+		$(".top_menu").slideToggle();
+		return false;
+	});
+
+	$(".sect_2_wrap").equalHeights();
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
